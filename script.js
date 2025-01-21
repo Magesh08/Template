7e2412eb223047816bd6a778016717fc6b2833cf
Magesh08 <mageshvaran16@gmail.com>
2025-01-21 19:51:37 +0530

    responsiveness2

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
 const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-     const navLinks = document.querySelectorAll('.nav-links li');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-    burger.addEventListener('click',() => {
-        nav.classList.toggle('nav-active');
-        navLinks.forEach((link, index) => {
-           if(link.style.animation){
-             link.style.animation = '';
-          }else{
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
-          }
-         
-        });
-          // Burger Animation
-        burger.classList.toggle('toggle')
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
     });
+    // Burger Animation
+    burger.classList.toggle("toggle");
+  });
+
+  // Add event listener to each nav link
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      nav.classList.remove("nav-active"); // Close the sidebar
+          burger.classList.remove('toggle'); // Close burger animation
+          navLinks.forEach(link => {
+              link.style.animation = '';
+          }); // Remove the animation on every link
+    });
+  });
 };
 
 const handleScroll = () => {
-   const sections = document.querySelectorAll('section');
+  const sections = document.querySelectorAll("section");
 
-   sections.forEach(section => {
-       const sectionTop = section.offsetTop;
-       const windowHeight = window.innerHeight;
-       const triggerBottom = sectionTop - windowHeight + 200;
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const windowHeight = window.innerHeight;
+    const triggerBottom = sectionTop - windowHeight + 200;
 
-       if (window.scrollY > triggerBottom) {
-           section.classList.add('show');
-       } else {
-           section.classList.remove('show');
-       }
-   });
+    if (window.scrollY > triggerBottom) {
+      section.classList.add("show");
+    } else {
+      section.classList.remove("show");
+    }
+  });
 };
 
 navSlide();
-window.addEventListener('scroll', handleScroll);
+window.addEventListener("scroll", handleScroll);
